18e22711423487399bc3c69f20dc7751fcd3d109
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2026-05-14 14:53:59 +0530

    Add Utility - backgrounds

---
+++ utilities/src/backgrounds.ts
@@ -0,0 +1,32 @@
+export const bgSizeValues: Record<string, string> = {
+  auto: "auto",
+  cover: "cover",
+  cobtain: "container",
+};
+
+export const bgPositionValues: Record<string, string> = {
+  center: "center",
+  left: "left",
+  right: "right",
+  top: "top",
+  bottom: "bottom",
+  "left-bottom": "left bottom",
+  "right-bottom": "right-bottom",
+  "left-top": "left top",
+  "right-top": "right top",
+};
+
+export const bgRepeatValues: Record<string, string> = {
+  repeat: "repeat",
+  "no-repeat": "no-repeat",
+  "repeat-x": "repeat-x",
+  "repeat-y": "repeat-y",
+  "repeat-round": "round",
+  "repeat-space": "space",
+};
+
+export const bgAttachmentValues: Record<string, string> = {
+  fixed: "fixed",
+  local: "local",
+  scroll: "scroll",
+};
